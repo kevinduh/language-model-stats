9833e180e3939d97c932be8808300b20edc86117
suzyahyah <suzyahyah@gmail.com>
2022-05-26 16:53:46 -0400

    parse mono and multilingual table info

--- bin/compile.sh
+++ bin/compile.sh
@@ -7,6 +7,7 @@ CSVPATH='https://docs.google.com/spreadsheets/d/1CwFdeJlGGZU3w8pUTMV43EdyKcTPFoz
 wget --no-check-certificate -O interim/lm_stats.csv $CSVPATH
 python format_md.py
 
-csv2md interim/lm_stats.csv > interim/table.md
+csv2md interim/lm_stats_mono.csv > interim/table_mono.md
+csv2md interim/lm_stats_multi.csv > interim/table_multi.md
 
-cat interim/header.md interim/table.md interim/mid.md interim/legend.md > index.md
+cat interim/header.md interim/table.md interim/mid.md interim/table_multi.md interim/legend.md > index.md
